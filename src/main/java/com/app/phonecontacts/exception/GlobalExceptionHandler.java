72f187d34e34a6382a18bf53a799c233dbd86d4a
sadovenk0 <sadovenk0@icloud.com>
2023-07-07 00:55:50 +0300

    added exception handlers

--- src/main/java/com/app/phonecontacts/exception/GlobalExceptionHandler.java
+++ src/main/java/com/app/phonecontacts/exception/GlobalExceptionHandler.java
@@ -3,14 +3,70 @@ package com.app.phonecontacts.exception;
 import com.app.phonecontacts.model.dto.utils.ErrorResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
+import org.hibernate.exception.ConstraintViolationException;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.stream.Collectors;
 
 @Slf4j
 @ControllerAdvice
 public class GlobalExceptionHandler {
+    @ExceptionHandler(ResponseStatusException.class)
+    public ResponseEntity<ErrorResponse> handleResponseStatusException(
+            HttpServletRequest request,
+            ResponseStatusException ex
+    ) {
+        return getErrorResponse(request, HttpStatus.resolve(ex.getStatusCode().value()), ex.getReason());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleMethodArgumentNotValid(HttpServletRequest request, MethodArgumentNotValidException ex) {
+        String message = ex.getFieldErrors()
+                .stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .collect(Collectors.joining(", "));
+        return getErrorResponse(request, HttpStatus.BAD_REQUEST, message);
+    }
+
+    @ExceptionHandler(NullEntityReferenceException.class)
+    public ResponseEntity<ErrorResponse> handleNullEntityReferenceException(
+            HttpServletRequest request,
+            NullEntityReferenceException ex
+    ) {
+        return getErrorResponse(request, HttpStatus.BAD_REQUEST, ex.getMessage());
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<ErrorResponse> handleConstraintViolationException(
+            HttpServletRequest request,
+            ConstraintViolationException ex
+    ) {
+        return getErrorResponse(request, HttpStatus.BAD_REQUEST, ex.getMessage());
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<ErrorResponse> forbiddenServerErrorHandler(
+            HttpServletRequest request,
+            AccessDeniedException ex
+    ) {
+        return getErrorResponse(request, HttpStatus.FORBIDDEN, ex.getMessage());
+    }
+
+    @ExceptionHandler(DuplicateItemsException.class)
+    public ResponseEntity<ErrorResponse> handleDuplicateItemsException(
+            HttpServletRequest request,
+            Exception ex
+    ) {
+        return getErrorResponse(request, HttpStatus.CONFLICT, ex.getMessage());
+    }
+
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> handleException(
             HttpServletRequest request,
