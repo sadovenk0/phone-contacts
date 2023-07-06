bf6290039e2bb2339205b3b3ec15063a0a2aa9f7
Ihor Dzheva <idzheva@gmail.com>
2023-07-06 13:35:44 +0300

    SecurityConfig
    SecurityConfig:
    - PasswordEncoder via
    NoOpPasswordEncoder

--- src/main/java/com/app/phonecontacts/PhoneContactsApplication.java
+++ src/main/java/com/app/phonecontacts/PhoneContactsApplication.java
@@ -1,13 +1,31 @@
 package com.app.phonecontacts;
 
+import com.app.phonecontacts.model.User;
+import com.app.phonecontacts.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PhoneContactsApplication {
+public class PhoneContactsApplication implements CommandLineRunner {
+    private final UserRepository userRepository;
+    @Autowired
+    public PhoneContactsApplication(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(PhoneContactsApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        User user = new User();
+        user.setId(1L);
+        user.setLogin("mike");
+        user.setPassword("1111Mike");
+        user.setRole("USER");
+        userRepository.save(user);
+    }
 }
--- src/main/java/com/app/phonecontacts/config/SecurityConfig.java
+++ src/main/java/com/app/phonecontacts/config/SecurityConfig.java
@@ -4,15 +4,18 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 import static org.springframework.http.HttpStatus.UNAUTHORIZED;
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Slf4j
 @Configuration
@@ -29,7 +32,7 @@ public class SecurityConfig {
                         .requestMatchers("/auth/login", "/auth/registration", "/error").permitAll()
                         .anyRequest().authenticated()
                 )
-                .httpBasic(c -> c.authenticationEntryPoint(restAuthenticationEntryPoint()))
+                .httpBasic(withDefaults())
                 .sessionManagement(c -> c.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .build();
     }
@@ -45,6 +48,6 @@ public class SecurityConfig {
 
     @Bean
     PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+        return NoOpPasswordEncoder.getInstance();
     }
 }
