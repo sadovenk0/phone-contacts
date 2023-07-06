573c54dbbb3c7f44ac9e79058e55c5c2f6432b2b
Ihor Dzheva <idzheva@gmail.com>
2023-07-06 11:32:10 +0300

    User
    User

---
+++ src/main/java/com/app/phonecontacts/model/User.java
@@ -0,0 +1,36 @@
+package com.app.phonecontacts.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter @NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotEmpty(message = "Login must not be empty")
+    @Size(min = 2, max = 20, message = "The login must be between 2 and 20 characters long")
+    @Column(name = "login", nullable = false, unique = true)
+    private String login;
+
+    @Pattern(regexp = ".*[a-z].*",
+            message = "Must contain at least one lowercase letter")
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "role")
+    private String role;
+
+    //@OneToMany(mappedBy = "owner", cascade = CascadeType.REMOVE)
+    //private List<Contact> contacts;
+
+}
---
+++ src/main/java/com/app/phonecontacts/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.app.phonecontacts.repository;
+
+import com.app.phonecontacts.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByLogin(String login);
+}
