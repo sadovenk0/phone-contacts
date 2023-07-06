b632338c72c353fea1d26d2e71123097ca7cfc77
sadovenk0 <sadovenk0@icloud.com>
2023-07-06 12:43:55 +0300

    created ContactMapper (added utils methodes for mapping emails and numbers between entity and dtos)

---
+++ src/main/java/com/app/phonecontacts/model/dto/contact/ContactMapper.java
@@ -0,0 +1,35 @@
+package com.app.phonecontacts.model.dto.contact;
+
+import com.app.phonecontacts.model.entity.Contact;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+@Mapper(componentModel = "spring")
+public interface ContactMapper {
+    @Mapping(target = "emails", source = "task.emails")
+    ContactResponse contactToContactResponse(Contact task);
+
+    @Mapping(target = "emails", source = "emails", qualifiedByName = "listToString")
+    @Mapping(target = "numbers", source = "numbers", qualifiedByName = "listToString")
+    Contact contactRequestToContact(ContactRequest taskRequest);
+
+    @Named("listToString")
+    default String mapListToString(List<String> list) {
+        if (list == null || list.isEmpty()) {
+            return null;
+        }
+        return String.join(", ", list);
+    }
+
+    @Named("stringToList")
+    default List<String> mapStringToList(String line) {
+        if (line == null || line.isEmpty()) {
+            return null;
+        }
+        return Stream.of(line.split(",")).map(String::trim).toList();
+    }
+}
